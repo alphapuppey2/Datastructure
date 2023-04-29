1b2335fa245a2fe05f81f2145fefdcd517a0e752
Joshua Loui Soqueno <80311305+alphapuppey2@users.noreply.github.com>
2023-04-29 11:14:30 +0800

    Update ParialOrderedTree.c

--- ParialOrderedTree.c
+++ ParialOrderedTree.c
@@ -1,10 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define SIZE 10
 
 
 typedef struct POT
 {
-    int data;
-    struct POT * l;
-    struct POT * r;
-}pot , *potr;
+    int data[SIZE];
+    int lastIndx;
+}pot;
+
+
+void initRoot(pot *);
+
+int main(void){
+    
+    pot root;
+
+
+    // index 0 is always an Root val
+    initRoot(&root);
+
+
+    
+    
+    return 0;
+}
+void initRoot(pot * A){
+       A->lastIndx = 0;
+}
