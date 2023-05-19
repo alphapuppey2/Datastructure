4db92573cbcd54cec1cbb947473396f3e622aea5
Joshua Loui Soqueno <80311305+alphapuppey2@users.noreply.github.com>
2023-05-19 22:39:21 +0800

    update

--- Finals/Internet/I_dijkstra.c
+++ Finals/Internet/I_dijkstra.c
@@ -6,6 +6,7 @@ void Dijkstra(int Graph[MAX][MAX], int n, int start);
 
 void Dijkstra(int Graph[MAX][MAX], int n, int start) {
   int cost[MAX][MAX], distance[MAX], pred[MAX];
+  
   int visited[MAX], count, mindistance, nextnode, i, j;
 
   // Creating cost matrix
--- Finals/dijkstra_Soqs.c
+++ Finals/dijkstra_Soqs.c
@@ -9,45 +9,57 @@ typedef int graph[MAX][MAX];
 
 
 void initialized(graph A);
-void dijkstra(int graph[MAX][MAX] , int n , int start );
-void display(graph rep , int n, int start);
+void dijkstra(graph list ,graph cost,int distance[MAX], int n , int start );
+void display(int rep[MAX] , int n, int start);
 
 int main(void){
 
 
-    graph a;
+    graph matrix;
+    graph cost;
 
-    node start = 0; // first vertex
-    node n = 7;     // last vertex
+    int distance[MAX];
+
+    node start = 0; // node source => connect node to another
+    node n = 3;     // total Vertex
 
 
-    initialized(a);
+    initialized(matrix);
+    //all edges has 0 weight
 
-    // a[0][0] = 0;
-    // a[0][1] = 0;
-    // a[0][2] = 0;
-    // a[0][3] = 0;
-    // a[0][4] = 0;
-    // a[0][5] = 0; 
-    // a[0][6] = 0; 
+    matrix[0][2] = 1;
+    matrix[0][3] = 2;
+    
+    matrix[1][2] = 2;
+    matrix[1][5] = 3;
+    
+    matrix[2][0] = 1;
+    matrix[2][1] = 2;
+    matrix[2][3] = 1;
+    matrix[2][4] = 3;
 
-    display(a , n , start);
+
+
+    printf("HELLObefore");
+    dijkstra(matrix , cost ,distance, n,start);
+    printf("HELLO");
+    display( distance , n , start);
 
 
 
     return 0;
 }
-void display(graph rep , int n , int start){
+void display(int rep[MAX] , int n , int start){
 
     int x , z;
     printf("The Graph:: \n");
 
-    for(x = start; x < n; x++){
-        for ( z = 0; z < n; z++)
-        {
-            printf("Graph[%d][%d] :: %d\n" ,x,z, rep[x][z]);
-        }
+    for(x = start; x < n; x++)
+    {
+        if(x != start){
+            printf("distance from source to %d :: %d\n" ,x ,rep[x]);
         printf("\n");
+        }
     }
 
 
@@ -75,37 +87,74 @@ void initialized(graph A){
 
 
 }
-void dijkstra(graph G , int n , int start ){
+/*
+    n is the total vertex in a graph
+    distance[] is used to know the distance between source to vertex
+*/
+void dijkstra(graph G ,graph costs, int distance[MAX],int n , int start ){
 
 
-    graph costs;
-    graph distance;
-    graph pred;
+    int pred[MAX];
 
     int visited[MAX];
     int count, minDistance , nextNode;
     int z,x;
 
-    // Weights
+    // costMatrix
     for (z = 0; z < n; z++)
     {
         for (x = 0; x < n; x++)
         {
-            if(G[z][x] == 0){
-                costs[z][x] = infinity;
-            }
-            else{
-                costs[z][x] = G[z][x];
-            }
+            //if edge weight == 0
+            costs[z][x] = (G[z][x] == 0) ? infinity :  G[z][x];
+
         }
         
     }
-    
 
+    //initial state for dijkstra algorithm
+    // start within the start Node / the source Node
+    for (z = 0; z < n; z++)
+    {
+        /* code */
+        distance[z] = costs[start][z];
+        pred[z] = start;
+        visited[z] = 0;
+    }
 
+    distance[start] = 0; // source pointing itself is always 0
+    visited[start] = 1; // we are visited the vertex which is the Source Node
 
+    //this is where we start comparing other vertex
+    //count = 1; 
 
+    for(count = 1; count <= n ; count++){
+        //finding the min weight of a nextnode 
+        for(z =0;z < n ;z++ ){
+            if(distance[z] < minDistance && !visited[z]){
+                minDistance = distance[z];
+                nextNode = z;
+            }
+        }
+        // track in visiting each node from the source
+        visited[nextNode] = 1;
+        for (z = 0; z < n; z++)
+        {
+            /* code */
+            if (!visited[z])
+            {
+               if(minDistance + costs[nextNode][z] < distance[z]){
+                 /* distance is for the source to other vertex */
+                distance[z] = minDistance + costs[nextNode][z];
+                pred[z] = nextNode;
+               }
+            }
+        } 
+    }  
 }
+
+
+
 
 
 
